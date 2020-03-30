1e7f220447bfb21008f38b54eba5b66e92cb0df8
shtarbanov <alims@media.mit.edu>
2020-03-30 03:37:33 -0400

    LED Control example

---
+++ LED Control via WebBLE/Embedded/Embedded.ino
@@ -0,0 +1,114 @@
+/*This code sets up the nrf52832 Feather as a peripheral with one custom service and one characteristic 
+The characteristic has read and write permissions, and contains a 2-byte value, which we map to the state of the LEDs.
+Whenver the value of the characteristic is changed by another device (a central), the LED states also change accoridngly.
+
+Whenever the value of the characteristic changes, a callback function is executed that reads the
+new value and then sets the state of the LEDs accordingly. Notice that thee main loop is completely empty.
+We are not doing any polling, but rather everything is event driven and based purely on callback functions.
+
+That is very nice with this approach where we are working directly with the characteristic, is that we don't
+have to worry about matching baud rates, we don't have to constantly be polling and checking for data which 
+saves energy, and we have full flexibility in what we want to do. This approach is way better than BLE UART.
+*/
+
+/*  TODO: Is there a way to simplify how UUID is assigned?
+ *  TODO: Look more indepth into the advertising settings, flags, permissions.
+ *  TODO: How are my communication parameters, tx power, etc.
+ *  TODO: Can you add encription?
+ */
+#include <bluefruit.h>
+#define DEVICE_NAME "nrf52 LED Control"// Device Name: Maximum 30 bytes
+#define LED_SERVICE_UUID "a3941db0-a97c-4cf1-943f-a25ff9ba40cd"
+#define LED_CHARACTERISTIC_UUID "5b8c0ab6-a058-4684-b2b6-4a0a692e2d45"
+
+#define redLed 17 //red led
+#define blueLed 19
+
+uint8_t ledServiceUUID[16];
+uint8_t ledCharacteristicUUID[16];
+
+BLEService ledService;
+BLECharacteristic ledCharacteristic;
+
+void setup() {
+  pinMode(redLed, OUTPUT);
+  pinMode(blueLed, OUTPUT);
+  Bluefruit.begin();
+  Bluefruit.setName(DEVICE_NAME);
+  Bluefruit.Periph.setConnectCallback(connect_callback);
+  Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
+  setupServices();
+  startAdvertising();
+}
+
+void loop() {
+}
+
+void setupServices(void) {
+  // Convert String UUID to raw UUID bytes
+  strUUID2Bytes(LED_SERVICE_UUID, ledServiceUUID);
+  strUUID2Bytes(LED_CHARACTERISTIC_UUID, ledCharacteristicUUID);
+
+  ledService = BLEService(ledServiceUUID);
+  ledService.begin();
+
+  ledCharacteristic = BLECharacteristic(ledCharacteristicUUID);
+  ledCharacteristic.setProperties(CHR_PROPS_WRITE | CHR_PROPS_READ);
+  ledCharacteristic.setWriteCallback(getCharacteristicValueAndSetLEDs);
+  ledCharacteristic.setPermission(SECMODE_ENC_NO_MITM, SECMODE_ENC_NO_MITM);
+  ledCharacteristic.setFixedLen(2);
+  ledCharacteristic.begin();
+}
+
+void startAdvertising(void) {
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+  Bluefruit.Advertising.addService(ledService);
+  Bluefruit.ScanResponse.addName();
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.start(0);
+}
+
+//###########################---Callbacks---##################################
+
+//This is executed when a central device writes to the characteristic.
+void getCharacteristicValueAndSetLEDs(uint16_t conn_hdl, BLECharacteristic* chr, uint8_t* data, uint16_t len) {
+  if(len==2){
+    int firstByte = data[0];
+    char secondByte = data[1];
+    digitalWrite(redLed, firstByte);
+    digitalWrite(blueLed, secondByte);
+  }
+}
+void connect_callback(uint16_t conn_handle){
+  //Set the initial value of the characteristic and the initial states of the LEDs to that value.
+  uint8_t chrVals[] = {0x01,0x01};
+  digitalWrite(redLed,chrVals[0]);
+  digitalWrite(blueLed,chrVals[1]);
+  ledCharacteristic.write(chrVals,2);
+}
+void disconnect_callback(uint16_t conn_handle, uint8_t reason){
+  digitalWrite(redLed,LOW);
+  digitalWrite(blueLed,LOW);
+}
+
+//###########################---UUID Converter---#################################
+void strUUID2Bytes(String strUUID, uint8_t binUUID[]) {
+  String hexString = String(strUUID);
+  hexString.replace("-", "");
+
+  for (int i = 16; i != 0 ; i--) {
+    binUUID[i - 1] = hex2c(hexString[(16 - i) * 2], hexString[((16 - i) * 2) + 1]);
+  }
+}
+
+char hex2c(char c1, char c2) {
+  return (nibble2c(c1) << 4) + nibble2c(c2);
+}
+
+char nibble2c(char c) {
+  if ((c >= '0') && (c <= '9')) return c - '0';
+  if ((c >= 'A') && (c <= 'F')) return c + 10 - 'A';
+  if ((c >= 'a') && (c <= 'f')) return c + 10 - 'a';
+  return 0;
+}
---
+++ LED Control via WebBLE/WebApp/nrf_led_button_service.js
@@ -0,0 +1,90 @@
+/* 
+  This example shows how to control the state of each of the two LEDs on the nrf52832 Feather
+  board. The microcontorller has been set up so that is has one custom service with a single
+  characteristic that holds a 2-byte value. The two bytes in the characteristic correspond to
+  the state of the two LEDs on the board. The characteristic has a read and write persmisions.
+    In this JavaScript code, we are connecting to the device and reading the current value of
+  the characteristic. Then we are toggling one of the two bytes depending on which button the
+  user presses on the screen, which in turn causes the corresponding LED to toggle as well. 
+*/
+'use strict'
+
+const serviceUUID = 'a3941db0-a97c-4cf1-943f-a25ff9ba40cd';
+const ledCharacteristicUUID = '5b8c0ab6-a058-4684-b2b6-4a0a692e2d45';
+
+let bleDevice;
+let bleServer;
+let bleService;
+let ledCharacteristic;
+let valueArray;
+let stateRed = true;
+let stateBlue = true;
+
+window.onload = function(){
+  document.querySelector('#connect').addEventListener('click', connect);
+  document.querySelector('#disconnect').addEventListener('click', onDisconnectButtonClick);
+  document.querySelector('#red').addEventListener('click', toggleRed);
+  document.querySelector('#blue').addEventListener('click', toggleBlue);
+  document.querySelector('#read').addEventListener('click', readCharacteristicValue);
+};
+
+async function connect() {  
+  try{
+    bleDevice = await navigator.bluetooth.requestDevice({
+          filters: [{namePrefix: 'nrf52'}],
+          optionalServices: [serviceUUID]
+        });
+    bleServer = await bleDevice.gatt.connect();
+    bleService = await bleServer.getPrimaryService(serviceUUID);
+    ledCharacteristic = await bleService.getCharacteristic(ledCharacteristicUUID);
+    
+    //##################
+    //Now we want to read the current value of the characteristic and set our LEDstate
+    //variables in JavaScript to match those from the characteristic.
+    //##################
+
+    let valueDataView = await ledCharacteristic.readValue(); //returns a DataView.
+    //If you didn't know that object is of type "DataView" you could just do
+    //console.log(valueDataView) and then you will see all infor about this variable in the console.
+
+    //We now convert the DataView to TypedArray so we can use array notation to access the data.
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DataView/buffer
+    valueArray = new Uint8Array(valueDataView.buffer);
+    log("Connected! The value is: \nQ1=" + valueArray[1] + " Q0=" + valueArray[0]);
+    stateBlue = valueArray[1];
+    stateRed = valueArray[0];
+  }
+  catch(error){
+    log("Ouch! " + error);
+  }
+}
+function onDisconnectButtonClick() {
+  if (!bleDevice) {
+    log('No device found');
+  }
+  else if (bleDevice.gatt.connected) {
+    log('Disconnecting');
+    bleDevice.gatt.disconnect();
+  } 
+  else {
+    log('Device already disconnected');
+  }
+}
+async function readCharacteristicValue(){
+  let val = await ledCharacteristic.readValue(); //this returns a DataView
+  log("Q1=" + val.getUint8(1) + " Q0=" + val.getUint8(0));
+}
+async function toggleRed(){
+    valueArray[0] = (stateRed) ? 0x00 : 0x01;
+    stateRed = !stateRed;
+    await ledCharacteristic.writeValue(valueArray);
+}
+async function toggleBlue(){
+    valueArray[1] = (stateBlue) ? 0x00 : 0x01;
+    stateBlue = !stateBlue;
+    await ledCharacteristic.writeValue(valueArray);
+}
+function log(text) {
+    console.log(text);
+    document.querySelector('#log').textContent += text + '\n';
+}

1615dfcbfa87157c8cceddfbf5e263f61621b312
shtarbanov <alims@media.mit.edu>
2021-08-31 12:09:58 -0400

    Removed obsolete files

--- FlowIO Examples/FlowIO_controlService/WebApp/toBinaryString.js
+++
@@ -1,9 +0,0 @@
-//We are extending the Number class to display leading zeros
-//gist.github.com/endel/321925f6cafa25bbfbde
-Number.prototype.toBinaryString = function(size) {
-  var s = this.toString(2);
-  while (s.length < size) {
-    s = "0" + s;
-  }
-  return s;
-}
